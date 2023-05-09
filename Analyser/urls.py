5aa448f5ac3cad778f4f5b21f6891192d7be85f2
Shubham Shirse <svshirse@gmail.om>
2023-05-09 11:43:41 +0530

    Added POSTMAN collection and Get Result URL

--- Analyser/urls.py
+++ Analyser/urls.py
@@ -6,4 +6,5 @@ VERSION = ""
 
 urlpatterns = [
     path("scan_url/", views.scan_url, name=VERSION + "scan_url"),
+    url(r'^scan_url/(?P<pk>[0-9]+)/$', views.scan_url_detail, name='scan_url_detail'),  # GET, delete
 ]
